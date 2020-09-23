6e3741ce17cf5eb64c6c792d0587237477311ccc
Vanderlei Alves da Silva <Vanderlei.Alves-Da-Silva@jsmd-group.com>
2020-09-23 16:42:51 +0200

    Fix multiple calls to react render

--- src/WebComponent.tsx
+++ src/WebComponent.tsx
@@ -4,18 +4,31 @@ import { ProductList } from "./ProductList";
 
 export class ProductListWebComponent extends HTMLElement {
   mountPoint: HTMLDivElement;
+  isRendered: boolean;
 
   constructor() {
     super();
     this.mountPoint = document.createElement("div");
     this.attachShadow({ mode: "open" }).appendChild(this.mountPoint);
+    this.isRendered = false;
   }
 
   static get observedAttributes() {
-    return ["name", "description", "amount"];
+    return ["title", "description", "amount"];
+  }
+
+  connectedCallback() {
+    this.render();
+    this.isRendered = true;
   }
 
   attributeChangedCallback() {
+    if (this.isRendered) {
+      this.render();
+    }
+  }
+
+  private render() {
     const title = this.getAttribute("title") || "";
     const description = this.getAttribute("description") || "";
     const amount = parseInt(this.getAttribute("amount") || "1");
