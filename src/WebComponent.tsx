093a2e431d9f81d016a7335453b01d7f29a8bb5b
Vanderlei Alves da Silva <Vanderlei.Alves-Da-Silva@jsmd-group.com>
2020-09-29 08:48:27 +0200

    custom styles with template tag

--- src/WebComponent.tsx
+++ src/WebComponent.tsx
@@ -4,32 +4,72 @@ import { ProductList } from "./ProductList";
 
 export class ProductListWebComponent extends HTMLElement {
   mountPoint: HTMLDivElement;
+  stylePoint: HTMLDivElement;
   isRendered: boolean;
 
   constructor() {
     super();
     this.mountPoint = document.createElement("div");
-    this.attachShadow({ mode: "open" }).appendChild(this.mountPoint);
+    this.stylePoint = document.createElement("div");
+
+    this.attachShadow({ mode: "open" }).append(
+      this.mountPoint,
+      this.stylePoint
+    );
+
     this.isRendered = false;
   }
 
   static get observedAttributes() {
-    return ["title", "description", "amount"];
+    return ["title", "description", "amount", "template-id"];
   }
 
   connectedCallback() {
+    this.template();
     this.render();
     this.isRendered = true;
   }
 
   disconnectedCallback() {
-    ReactDOM.unmountComponentAtNode(this.mountPoint);
+    this.shadowRoot && ReactDOM.unmountComponentAtNode(this.shadowRoot);
   }
 
-  attributeChangedCallback() {
-    if (this.isRendered) {
-      this.render();
+  attributeChangedCallback(attr: string) {
+    if (!this.isRendered) return;
+
+    if (attr === "template-id") {
+      return this.template();
     }
+
+    this.render();
+  }
+
+  private template() {
+    const templateElement = document.getElementById(
+      this.getAttribute("template-id") || ""
+    ) as HTMLTemplateElement;
+
+    if (!(templateElement instanceof HTMLTemplateElement)) return;
+
+    const update = () => {
+      this.stylePoint.innerHTML = "";
+
+      this.stylePoint.append(
+        templateElement.content.querySelector("style")?.cloneNode(true) || "",
+        templateElement.content.querySelector("link")?.cloneNode(true) || ""
+      );
+    };
+
+    const observer = new MutationObserver(update);
+
+    observer.observe(templateElement.content, {
+      attributes: true,
+      childList: true,
+      subtree: true,
+      characterData: true,
+    });
+
+    update();
   }
 
   private render() {
